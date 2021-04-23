d74b004938b7381b83a23c733cc4a9a9ec7a5413
maruthiravuri <maruthiravuri@gmail.com>
2021-04-22 20:17:26 -0700

    changes

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -11,6 +11,15 @@ export const getPosts = async (req,res)=> {
     }
 }
 
-export const createPost = (req,res)=> {
+export const createPost = async (req,res)=> {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        //https://www.restapitutorial.com/httpstatuscodes.html
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message :error.message});
+    }
     res.send('Post Creted!');
 }
